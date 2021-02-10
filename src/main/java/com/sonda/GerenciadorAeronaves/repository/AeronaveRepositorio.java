6c27bb729f77adb70121489abf4d2ab9b4f0dfba
Jean Pierre <jean.pierre988@hotmail.com>
2021-02-09 22:28:34 -0300

    Update AeronaveRepositorio.java

--- src/main/java/com/sonda/GerenciadorAeronaves/repository/AeronaveRepositorio.java
+++ src/main/java/com/sonda/GerenciadorAeronaves/repository/AeronaveRepositorio.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AeronaveRepositorio extends JpaRepository<Aeronave, Long> {
-    Integer countDistinctMarca();
+//    Integer countDistinctMarca()
 }
